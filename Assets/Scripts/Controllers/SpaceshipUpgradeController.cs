778bf53434da67a4e868cad30dba31c97287607d
Nishant Kumar <nishant4498@gmail.com>
2016-10-15 15:43:05 -0700

    Added changes for the spaceship upgrade menu.

---
+++ Assets/Scripts/Controllers/SpaceshipUpgradeController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SpaceshipUpgradeController : MonoBehaviour {	
+	public Text spaceshipHP;
+    public Text gunHP;
+    public Text minMedals;
+	public Text price;
+
+	void Start() {
+
+        spaceshipHP.text =  GameController.Instance.shop.spaceships[1].hp.ToString();
+        gunHP.text = GameController.Instance.shop.spaceships[1].primaryGun.hitPoint.ToString();
+        minMedals.text = GameController.Instance.shop.spaceships[1].minMedals.ToString();
+        price.text = GameController.Instance.shop.spaceships[1].price.ToString();
+	}	
+}
