dde40e03fd88c253c91be0753753130d55b72e2f
Sridevi Gadiraju <gadiraju@usc.edu>
2016-10-16 14:49:46 -0700

    Initial commit

---
+++ Assets/Scripts/Controllers/GunBuyController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+
+public class GunBuyController: MonoBehaviour
+{
+
+	public GameObject GunPanel;
+	public GameObject GunBuyListItemPrefab;
+
+	public List<Gun> GunsToBuy { get; set; }
+	public List<GameObject> GunGameObjects { get; set; }
+
+	void Start() {
+
+		List<Gun> ShopGuns = GameController.Instance.shop.guns;
+		List<Gun> PlayerGuns = GameController.Instance.profile.guns;
+
+		GunsToBuy = ShopGuns.Except(PlayerGuns).ToList();
+		Debug.Log (GunsToBuy.Count);
+
+		GameObject gunPrefabItem = (GameObject) Resources.Load("GunBuyListItem", typeof(GameObject));
+
+		GunGameObjects = new List<GameObject> ();
+
+		foreach(Gun gun in GunsToBuy){
+
+			GameObject gunItem = Instantiate(gunPrefabItem) as GameObject;
+			GunBuyItemController controller = gunItem.GetComponent<GunBuyItemController>();
+
+			GunGameObjects.Add (gunItem);
+
+			//controller.Image.sprite = gun.image;
+			controller.gun = gun;
+			controller.gunBuyController = this;
+
+			gunItem.transform.SetParent(GunPanel.transform);
+			gunItem.transform.localScale = Vector3.one;
+		}
+	}
+
+	public void Buy() {
+
+
+		//UserProfile.Save ();
+	}
+		
+}
+
+
---
+++ Assets/Scripts/Controllers/GunBuyItemController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GunBuyItemController: MonoBehaviour
+{
+	public Image Image;
+	public Text HPText;
+	public Text AmmoText;
+	public Text Name;
+	public Button Buy;
+
+	public Gun gun { get; set; }
+
+	public GunBuyController gunBuyController  { get; set; }
+
+	void Start() {
+		Name.text = gun.name;
+		HPText.text = "Hit Point : " + gun.hitPoint;
+		AmmoText.text = "Ammo : " + gun.ammo;
+
+		Button buyBtn = GetComponent<Button> ();
+	}
+		
+}
