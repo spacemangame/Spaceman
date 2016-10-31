c0c65b6ef5ac1c70d830011df65fff12b572ce6a
pnisarg <pnisarg26@yahoo.com>
2016-10-31 14:52:37 -0700

    added booster refill logic

--- Assets/BoostButton.cs
+++ Assets/BoostButton.cs
@@ -7,6 +7,7 @@ public class BoostButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 
 	private bool touched;
 	private int pointerID;
+	private float boostRefillTime = 3.0f;
 
 	public bool boost { set; get;}
 
@@ -19,13 +20,24 @@ public class BoostButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 			touched = true;
 			pointerID = ped.pointerId;
 			boost = true;
+			Invoke ("disableBoost", 3.0f); //boost effect will last for 3s
+			gameObject.SetActive(false); //hide boost button
+			Invoke("enableBoostButton", 6.0f);
 		}
 	}
 
+	private void enableBoostButton(){
+		gameObject.SetActive (true); //show boost button
+	}
+	private void disableBoost(){
+		boost = false;
+		touched = false;
+	}
+
 	public void OnPointerUp(PointerEventData ped) {
-		if (ped.pointerId == pointerID) {
-			touched = false;
-			boost = false;
-		}
+//		if (ped.pointerId == pointerID) {
+//			touched = false;
+//			boost = false;
+//		}
 	}
 }
